44251be90c057c0d33e8d0032c5d670e237e4222
Rehamus <161007461+Rehamus@users.noreply.github.com>
2024-04-12 14:09:44 +0900

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ function login_test() {
 
   if (sessionStorage.getItem("login_o") == null) {
     sessionStorage.setItem("login_o", 0);
+    
   }else if (sessionStorage.getItem("login_o") != 0) {
     $('.ID_log').hide();
   }
@@ -52,3 +53,39 @@ $('#logout').click(function () {
 });
 /*로그아웃 끝 */
 
+// 접었다 폈다
+$('#contents_view > li').children('button').click(function () {
+  let onoff = $(this).nextAll().children('div');
+
+  if( onoff.hasClass( "hidden")){
+    onoff.removeClass('hidden')
+  }else{
+    onoff.addClass('hidden')
+  }
+
+})
+
+// 매뉴 따라오게 만들기
+$(document).ready(function() {
+  var $floatingElement = $('#box2');
+  var floatingStart = 450; // 요소가 이동을 시작할 스크롤 위치를 설정합니다.
+
+  $(window).scroll(function() {
+      var scrollPosition = $(this).scrollTop();
+
+      // 만약 스크롤 위치가 시작 위치보다 크거나 같다면,
+      if (scrollPosition >= floatingStart) {
+          // 요소의 위치를 조정하여 스크롤과 함께 이동하도록 합니다.
+          $floatingElement.css({
+              'position': 'fixed',
+              'top': '0'
+          });
+      } else {
+          // 아니면 원래 위치로 되돌립니다.
+          $floatingElement.css({
+              'position': 'absolute',
+              'top': '448px' // 시작 위치
+          });
+      }
+  });
+});
